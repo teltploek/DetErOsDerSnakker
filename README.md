7642c99da2b50b327d426afda171186b3e020c78
teltploek <brian.frisch@gmail.com>
2013-05-02 22:03:18 +0200

    added grunt to dev only

--- README.md
+++ README.md
@@ -30,8 +30,7 @@ Right now everything is work in progress, so don't expect anything to work yet.
 
 ## TODOs
 
-  * We might need to make request a child_process 
-  * Add Jekyll blog with bookmarklet instructions
+  * We might need to make request a child_process
   * Show "Chrome desktop only" splash screen for other browsers
 
 ## Known issues
--- app.js
+++ app.js
@@ -4,21 +4,8 @@
  */
 
 var express = require('express'),
-    // fs = require('fs'),
-    cp = require('child_process'), // used for grunt 
     routes = require('./routes'),
     Nationen = require('./routes/nationen.js');
-
-  // setup grunt as a child process
-var grunt = cp.spawn('grunt.cmd', ['default', 'watch']);
-
-// TODO: read package.json data with fs to show project status (version etc.) in index footer
-// var packageJSON = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-
-grunt.stdout.on('data', function(data) {
-  // relay output to console
-  console.log("%s", data)
-})
 
 var app = module.exports = express();
 var server = require('http').createServer(app);
@@ -42,6 +29,16 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
+  var cp = require('child_process'); // used for grunt 
+
+  // setup grunt as a child process
+  var grunt = cp.spawn('grunt.cmd', ['default', 'watch']);
+
+  grunt.stdout.on('data', function(data) {
+    // relay output to console
+    console.log("%s", data)
+  });
+
   process.env.DBSTR = 'mongodb://localhost/DetErOsDerSnakker';
   
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
